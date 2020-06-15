0358ff208353d259091dc82447666ab8e27c8275
Кирилл <kirillmorgunov@mail.ru>
2020-06-15 16:25:19 +0300

    исправил slider js, доработал блок programms.

--- source/js/scripts/programms.js
+++ source/js/scripts/programms.js
@@ -2,6 +2,11 @@
 var tabContainer = document.querySelector(".jsTabs");
 var tabsList = document.querySelectorAll(".programms__tab");
 var tabs = Array.from(tabsList);
+var header = document.querySelector(".jsHeader");
+var paragraph1 = document.querySelector(".jsP1");
+var paragraph2 = document.querySelector(".jsP2");
+var textCap = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto cupiditate porro iste natus beatae ipsam, illum vitae sequi eius reiciendis at repellat quo perspiciatis corrupti officia unde quis exercitationem doloribus!";
+
 
 if (tabContainer) {
   tabContainer.addEventListener("click", function(evt) {
@@ -12,6 +17,29 @@ if (tabContainer) {
       el.classList.remove("programms__tab--active");
     });
     tab.classList.add("programms__tab--active");
-  })
+
+    switch(tab.dataset.info) {
+      case "culture": header.textContent = "Общие";
+      paragraph1.textContent = textCap;
+      paragraph2.textContent = textCap;
+      break;
+      case "study": header.textContent = "Академические";
+      paragraph1.textContent = "Провести семестр или год за рубежом, знакомясь с различными культурами и идеями, традициями и стилем жизни — вот что такое учеба за границей! Израиль — это не только центр религиозного мира, это также академический центр, живая лаборатория идей и творческого исследования.";
+      paragraph2.textContent = "Может быть, Вы заинтересованы в изучении социологии, мира, юриспруденции, биологии, сравнительной религии, законодательного и делового администрирования или искусства? Здесь, в Израиле, Вы сможете изучить все это в удивительной университетской среде.";
+      break;
+      case "business": header.textContent = "Стажировки";
+      paragraph1.textContent = textCap;
+      paragraph2.textContent = textCap;
+      break;
+      case "volonteur": header.textContent = "Волонтёрство";
+      paragraph1.textContent = textCap;
+      paragraph2.textContent = textCap;
+      break;
+      case "religion": header.textContent = "Религиозные";
+      paragraph1.textContent = textCap;
+      paragraph2.textContent = textCap;
+      break;
+    }
+  });
 }
 }());
--- source/js/scripts/slider.js
+++ source/js/scripts/slider.js
@@ -1,9 +1,10 @@
 (function () {
   'use strict';
   var slider = document.querySelector(".slider");
-  var sliderList = document.querySelectorAll(".programms__tab");
+  var sliderList = document.querySelectorAll(".slider__item");
   var sliders = Array.from(sliderList);
   var image = document.querySelector(".jsImg");
+  var description = document.querySelector(".jsGalleryP");
 
   if (slider) {
     console.log(slider);
@@ -18,6 +19,24 @@
           el.classList.remove("slider__item--active");
         });
         dot.classList.add("slider__item--active");
+
+        switch(dot.dataset.info) {
+          case "excursion": image.src = "img/excursion-mobile.png";
+          description.textContent = "Экскурсии по Израилю и знакомство с его историей"
+          break;
+          case "Hebrew": image.src = "img/Hebrew-mobile.png";
+          description.textContent = "Изучение иврита"
+          break;
+          case "profession": image.src = "img/profession-mobile.png";
+          description.textContent = "Обучение современным навыкам и профессиям"
+          break;
+          case "artprojects": image.src = "img/artprojects-mobile.png";
+          description.textContent = "Совместные творческие проекты с израильской молодёжью"
+          break;
+          case "leadership": image.src = "img/leadership-mobile.png";
+          description.textContent = "Развитие лидерства"
+          break;
+        }
       });
     });
   }
